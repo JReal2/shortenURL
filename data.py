85fc454b7dea53b5934999dad368f4045766737a
JRLee <49389212+JReal2@users.noreply.github.com>
2023-07-26 10:37:14 +0900

    add feature redirect

--- data.py
+++ data.py
@@ -1,9 +1,9 @@
 _data = {}
 
 
-def set(short_token, origin_url):
-    _data[short_token] = origin_url
-
-
 def get(short_token):
     return _data.get(short_token)
+
+
+def set(short_token,orgin_url):
+    _data[short_token] = orgin_url
